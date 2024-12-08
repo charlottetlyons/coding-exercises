219a509c1fc440b7b05f5da333b8cd41619e2642
charlottetlyons <charlottetlyons@gmail.com>
2024-12-08 10:03:27 -0600

    Autostyling fixes, encapsulate linked list tests, and implement linked list bubble sort

--- fun.py
+++ fun.py
@@ -1,6 +1,6 @@
 #  TEST UTILITY FUNCTIONS
 def format_test_result(result):
-    return '\033[32mPass\033[0m' if result else '\033[31mFail\033[0m'
+    return "\033[32mPass\033[0m" if result else "\033[31mFail\033[0m"
 
 # FUNCTIONS
 def access_index(some_list):
@@ -30,7 +30,7 @@ class LinkedList:
     def pop_first(self):
         if self.length == 0:
             return None
-        
+
         temp = self.head
 
         if self.length == 1:
@@ -117,7 +117,7 @@ class LinkedList:
         if index == self.length:
             return self.append(value)
         new_node = Node(value)
-        temp = self.get(index-1)
+        temp = self.get(index - 1)
         new_node.next = temp.next
         temp.next = new_node
         self.length += 1
@@ -130,7 +130,7 @@ class LinkedList:
             return self.pop_first()
         elif index == self.length - 1:
             return self.pop()
-        prev = self.get(index-1)
+        prev = self.get(index - 1)
         temp = prev.next
         prev.next = temp.next
         temp.next = None
@@ -173,32 +173,33 @@ class LinkedList:
             slow = slow.next
             fast = fast.next.next
         return slow
-    
+
     def kth_node_from_tail(self, k):
         slow = fast = self.head
         for _ in range(k):
             if not fast:
                 return None
             fast = fast.next
-                
+
         while fast:
             fast = fast.next
             slow = slow.next
         return slow
-    
+
     def reverse_between(self, m, n):
         if self.length == 0:
             return None
-        
+
         dummy = Node(0)
         dummy.next = self.head
         prev = dummy
 
         for _ in range(m):
             prev = prev.next
-            
+
         current = prev.next
-        for _ in range(n-m):
+
+        for _ in range(n - m):
             after = current.next
             current.next = after.next
             after.next = prev.next
@@ -206,23 +207,23 @@ class LinkedList:
         self.head = dummy.next
 
     def partition_list(self, x):
-        current = self.head
         dummy1 = Node(0)
         dummy2 = Node(0)
         prev1 = dummy1
         prev2 = dummy2
+        current = self.head
 
         while current:
             if current.value < x:
                 prev1.next = current
-                prev1 = prev1.next
+                prev1 = current
             else:
                 prev2.next = current
-                prev2 = prev2.next
+                prev2 = current
             current = current.next
 
         prev2.next = None
-        prev1.next = dummy2.next        
+        prev1.next = dummy2.next
         self.head = dummy1.next
 
     def remove_duplicates(self):
@@ -239,195 +240,219 @@ class LinkedList:
                 prev = temp
             temp = temp.next
 
+    def bubble_sort(self):
+        if self.length == 0:
+            return None
+
+        is_sorted = False
+
+        while not is_sorted:
+            current = self.head
+            compare = current.next
+            is_sorted = True
+
+            while compare:
+                if current.value > compare.value:
+                    current.value, compare.value = compare.value, current.value
+                    is_sorted = False
+                
+                current = compare
+                compare = compare.next
+
 # Test Cases
-def run_all_tests():
-    test_configs = [
-        ["test_linked_list_constructor", test_linked_list_constructor],
-        ["test_linked_list_pop_first", test_linked_list_pop_first],
-        ["test_linked_list_pop", test_linked_list_pop],
-        ["test_linked_list_prepend", test_linked_list_prepend],
-        ["test_linked_list_append", test_linked_list_append],
-        ["test_linked_list_print", test_linked_list_print],
-        ["test_linked_list_get", test_linked_list_get],
-        ["test_linked_list_set", test_linked_list_set],
-        ["test_linked_list_insert", test_linked_list_insert],
-        ["test_linked_list_remove", test_linked_list_remove],
-        ["test_linked_list_get_length", test_linked_list_get_length],
-        ["test_linked_list_functions_out_of_bounds", test_linked_list_functions_out_of_bounds],
-        ["test_max_value", test_max_value],
-        ["test_linked_list_reverse", test_linked_list_reverse],
-        ["test_linked_list_median_node", test_linked_list_median_node],
-        ["test_linked_list_reverse", test_linked_list_reverse],
-        ["test_linked_list_kth_node_from_tail", test_linked_list_kth_node_from_tail],
-        ["test_linked_list_reverse_between", test_linked_list_reverse_between],
-        ["test_linked_list_partition_list", test_linked_list_partition_list],
-        ["test_linked_list_remove_duplicates", test_linked_list_remove_duplicates]
-    ]
+class LinkedListTest:
+    def __init__(self):
+        self.test_configs = [
+            ["test_constructor", self.test_constructor],
+            ["test_pop_first", self.test_pop_first],
+            ["test_pop", self.test_pop],
+            ["test_prepend", self.test_prepend],
+            ["test_append", self.test_append],
+            ["test_print", self.test_print],
+            ["test_get", self.test_get],
+            ["test_set", self.test_set],
+            ["test_insert", self.test_insert],
+            ["test_remove", self.test_remove],
+            ["test_get_length", self.test_get_length],
+            ["test_functions_out_of_bounds", self.test_functions_out_of_bounds],
+            ["test_max_value", test_max_value],
+            ["test_reverse", self.test_reverse],
+            ["test_median_node", self.test_median_node],
+            ["test_reverse", self.test_reverse],
+            ["test_kth_node_from_tail", self.test_kth_node_from_tail],
+            ["test_reverse_between", self.test_reverse_between],
+            ["test_partition_list", self.test_partition_list],
+            ["test_remove_duplicates", self.test_remove_duplicates],
+            ["test_bubble_sort", self.test_bubble_sort],
+        ]
 
-    for test in test_configs:
-        test_result = format_test_result(test[1]())
-        print(f'{test[0]}: {test_result}')
+    def run_all_tests(self):
+        for test in self.test_configs:
+            test_result = format_test_result(test[1]())
+            print(f"{test[0]}: {test_result}")
 
-# Linked List Tests
-def initialize_test_linked_list(num_of_values=3, is_sorted=True, custom_list=None):
-    ll = LinkedList(1)
-    if custom_list:
-        ll.pop_first()
-        for value in custom_list:
-            ll.append(value)
-    else:
-        if is_sorted:
-            for value in range(2, num_of_values+1):
+    def initialize_test_linked_list(
+        self, num_of_values=3, is_sorted=True, custom_list=None
+    ):
+        ll = LinkedList(1)
+        if custom_list:
+            ll.pop_first()
+            for value in custom_list:
                 ll.append(value)
         else:
-            for value in range(num_of_values+1, 2):
-                ll.append(value)
-        
-    return ll 
+            if is_sorted:
+                for value in range(2, num_of_values + 1):
+                    ll.append(value)
+            else:
+                for value in range(num_of_values + 1, 2):
+                    ll.append(value)
 
-def test_linked_list_constructor():
-    ll = LinkedList(1)
-    return ll.get(0).value == 1 and ll.length == 1
+        return ll
 
-def test_linked_list_pop_first():
-    ll = initialize_test_linked_list()
-    return ll.pop_first().value == 1 and ll.length == 2
+    def test_constructor(self):
+        ll = LinkedList(1)
+        return ll.get(0).value == 1 and ll.length == 1
 
-def test_linked_list_pop():
-    ll = initialize_test_linked_list()
-    return ll.pop().value == 3 and ll.length == 2
+    def test_pop_first(self):
+        ll = self.initialize_test_linked_list()
+        return ll.pop_first().value == 1 and ll.length == 2
 
-def test_linked_list_prepend():
-    ll = initialize_test_linked_list()
-    ll.prepend(4)
-    return ll.get(0).value == 4 and ll.length == 4
+    def test_pop(self):
+        ll = self.initialize_test_linked_list()
+        return ll.pop().value == 3 and ll.length == 2
 
-def test_linked_list_append():
-    ll = initialize_test_linked_list()
-    ll.append(4)
-    return ll.get(3).value == 4 and ll.length == 4
+    def test_prepend(self):
+        ll = self.initialize_test_linked_list()
+        ll.prepend(4)
+        return ll.get(0).value == 4 and ll.length == 4
 
-# TODO: this seems annoying, do later
-def test_linked_list_print():
-    return True
+    def test_append(self):
+        ll = self.initialize_test_linked_list()
+        ll.append(4)
+        return ll.get(3).value == 4 and ll.length == 4
 
-def test_linked_list_get():
-    ll = initialize_test_linked_list()
-    return ll.get(1).value == 2
+    # TODO: this seems annoying, do later
+    def test_print(self):
+        return True
 
-def test_linked_list_functions_out_of_bounds():
-    ll = initialize_test_linked_list()
-    pre_index = -1
-    post_index = 4
-    test_value = 5
-    test_configs = [
-        dict(
-            test_name="test_linked_list_get_out_of_bounds", 
-            linked_list_function=ll.get, 
-            expected_result=None,
-            needs_value=False
-        ),
-        dict(
-            test_name="test_linked_list_set_out_of_bounds",
-            linked_list_function=ll.set, 
-            expected_result=False,
-            needs_value=True
-        ),
-        dict(
-            test_name="test_linked_list_insert_out_of_bounds",
-            linked_list_function=ll.insert,
-            expected_result=False,
-            needs_value=True
-        ),
-        dict(
-            test_name="test_linked_list_remove_out_of_bounds", 
-            linked_list_function=ll.remove, 
-            expected_result=None,
-            needs_value=False    
+    def test_get(self):
+        ll = self.initialize_test_linked_list()
+        return ll.get(1).value == 2
+
+    def test_functions_out_of_bounds(self):
+        ll = self.initialize_test_linked_list()
+        pre_index = -1
+        post_index = 4
+        test_value = 5
+        test_configs = [
+            dict(
+                test_name="test_linked_list_get_out_of_bounds",
+                linked_list_function=ll.get,
+                expected_result=None,
+                needs_value=False,
+            ),
+            dict(
+                test_name="test_linked_list_set_out_of_bounds",
+                linked_list_function=ll.set,
+                expected_result=False,
+                needs_value=True,
+            ),
+            dict(
+                test_name="test_linked_list_insert_out_of_bounds",
+                linked_list_function=ll.insert,
+                expected_result=False,
+                needs_value=True,
+            ),
+            dict(
+                test_name="test_linked_list_remove_out_of_bounds",
+                linked_list_function=ll.remove,
+                expected_result=None,
+                needs_value=False,
+            ),
+        ]
+
+        for test in test_configs:
+            if test["needs_value"]:
+                test_result = (
+                    test["linked_list_function"](pre_index, test_value)
+                    == test["expected_result"]
+                    and test["linked_list_function"](post_index, test_value)
+                    == test["expected_result"]
+                )
+            else:
+                test_result = (
+                    test["linked_list_function"](pre_index) == test["expected_result"]
+                    and test["linked_list_function"](post_index)
+                    == test["expected_result"]
+                )
+            print(f'{test["test_name"]}: {format_test_result(test_result)}')
+            if not test_result:
+                return False
+        return True
+
+    def test_set(self):
+        ll = self.initialize_test_linked_list()
+        ll.set(2, 5)
+        return ll.get(2).value == 5
+
+    # TODO: Test first and last insertion cases
+    def test_insert(self):
+        ll = self.initialize_test_linked_list()
+        ll.insert(1, 5)
+        return ll.get(1).value == 5 and ll.length == 4
+
+    def test_remove(self):
+        ll = self.initialize_test_linked_list()
+        return ll.remove(1).value == 2 and ll.length == 2
+
+    def test_get_length(self):
+        ll = self.initialize_test_linked_list()
+        return ll.get_length() == 3
+
+    def test_reverse(self):
+        ll = LinkedList(7)
+        for num in [3, 6, 2, 6, 1, 4]:
+            ll.append(num)
+        expected_order = [4, 1, 6, 2, 6, 3, 7]
+        ll.reverse()
+        return ll.values() == expected_order
+
+    def test_median_node(self):
+        ll = self.initialize_test_linked_list()
+        return ll.median_node().value == 2
+
+    def test_kth_node_from_tail(self):
+        ll = self.initialize_test_linked_list(num_of_values=5)
+        return (
+            ll.kth_node_from_tail(2).value == 4
+            and ll.kth_node_from_tail(1).value == 5
+            and ll.kth_node_from_tail(5).value == 1
+            and ll.kth_node_from_tail(0) == None
+            and ll.kth_node_from_tail(6) == None
         )
-    ]
 
-    for test in test_configs:
-        if test["needs_value"]:
-            test_result = test["linked_list_function"](pre_index, test_value) == test["expected_result"] and test["linked_list_function"](post_index, test_value) == test["expected_result"]
-        else:
-            test_result = test["linked_list_function"](pre_index) == test["expected_result"] and test["linked_list_function"](post_index) == test["expected_result"]
-        print(f'{test["test_name"]}: {format_test_result(test_result)}')
-        if not test_result:
-            return False
-    return True
+    def test_reverse_between(self):
+        ll = self.initialize_test_linked_list(num_of_values=5)
+        ll.reverse_between(1, 3)
+        return ll.values() == [1, 4, 3, 2, 5]
 
-def test_linked_list_set():
-    ll = initialize_test_linked_list()
-    ll.set(2, 5)
-    return ll.get(2).value == 5
+    def test_partition_list(self):
+        ll = self.initialize_test_linked_list(custom_list=[1, 3, 2, 3, 1, 14])
+        ll.partition_list(3)
+        return ll.values() == [1, 2, 1, 3, 3, 14]
 
-# TODO: Test first and last insertion cases
-def test_linked_list_insert():
-    ll = initialize_test_linked_list()
-    ll.insert(1, 5)
-    return ll.get(1).value == 5 and ll.length == 4
+    def test_values(self):
+        return self.initialize_test_linked_list().values() == [1, 2, 3, 4, 5]
 
-def test_linked_list_remove():
-    ll = initialize_test_linked_list()
-    return ll.remove(1).value == 2 and ll.length == 2
+    def test_remove_duplicates(self):
+        ll = self.initialize_test_linked_list(custom_list=[1, 1, 2, 3, 3, 3])
+        ll.remove_duplicates()
+        return ll.values() == [1, 2, 3]
 
-def test_linked_list_get_length():
-    ll = initialize_test_linked_list()
-    return ll.get_length() == 3
-
-def test_linked_list_reverse():
-    ll = LinkedList(7)
-    for num in [3, 6, 2, 6, 1, 4]:
-        ll.append(num)
-    expected_order = [4, 1, 6, 2, 6, 3, 7]
-    ll.reverse()
-    return ll.values() == expected_order
-
-def test_linked_list_sort():
-    # TODO: needed later
-    return
-
-    # ll = LinkedList(7)
-    # for num in [3, 6, 2, 6, 1, 4]:
-    #     ll.append(num)
-    # expected_order = [1, 2, 3, 4, 6, 6, 7]
-    # ll.sort()
-    # print(ll.values())
-    # print(expected_order)
-    # return ll.values() == expected_order
-
-def test_linked_list_median_node():
-    ll = initialize_test_linked_list()
-    return ll.median_node().value == 2
-
-def test_linked_list_kth_node_from_tail():
-    ll = initialize_test_linked_list(num_of_values=5)
-    return (
-        ll.kth_node_from_tail(2).value == 4 
-        and ll.kth_node_from_tail(1).value == 5 
-        and ll.kth_node_from_tail(5).value == 1 
-        and ll.kth_node_from_tail(0) == None 
-        and ll.kth_node_from_tail(6) == None
-    )
-
-def test_linked_list_reverse_between():
-    ll = initialize_test_linked_list(num_of_values=5)
-    ll.reverse_between(1, 3)
-    return ll.values() == [1,4,3,2,5] 
-
-def test_linked_list_partition_list():
-    ll = initialize_test_linked_list(custom_list=[1,3,2,3,1,14])
-    ll.partition_list(3)
-    return ll.values() == [1,2,1,3,3,14]
-    
-def test_linked_list_values():
-    return initialize_test_linked_list().values() == [1, 2, 3, 4, 5]
-
-def test_linked_list_remove_duplicates():
-    ll = initialize_test_linked_list(custom_list=[1, 1, 2, 3, 3, 3])
-    ll.remove_duplicates()
-    return ll.values() == [1, 2, 3]
+    def test_bubble_sort(self):
+        ll = self.initialize_test_linked_list(custom_list=[4, 2, 5, 3, 1, 6, 2])
+        ll.bubble_sort()
+        return ll.values() == [1, 2, 2, 3, 4, 5, 6]
 
 # Array Tests
 def test_max_value():
@@ -435,4 +460,5 @@ def test_max_value():
     return max_value(array) == 7
 
 # Test Execution
-run_all_tests()
+linked_list_test = LinkedListTest()
+linked_list_test.run_all_tests()
