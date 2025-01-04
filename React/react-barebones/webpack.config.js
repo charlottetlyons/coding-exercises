ab596b3c21de850295329bb7a192e847045b3069
Charlotte Lyons <charlottetlyons@gmail.com>
2025-01-04 16:53:57 -0600

    Copied react-barebones for react-playground

--- React/react-barebones/webpack.config.js
+++ React/react-barebones/webpack.config.js
@@ -23,7 +23,11 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx']
     },
-    plugins: [new HtmlWebpackPlugin({template: './public/index.html'})],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ],
     devServer: {
         static: path.join(__dirname, 'dist'),
         port: 3000,
