52075bd56201cc8b6c844f80b01bcef08d42d355
Charlotte Lyons <charlottetlyons@gmail.com>
2025-01-08 20:48:38 -0600

    Jest and React Testing Library

---
+++ React/react-testing-library/test/App.spec.js
@@ -0,0 +1,11 @@
+import App from "../src/App";
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+
+it('should say React!', () => {
+    render(<App/>);
+
+    const appElement = screen.getByText("React!")
+    
+    expect(appElement).toBeInTheDocument()
+})
