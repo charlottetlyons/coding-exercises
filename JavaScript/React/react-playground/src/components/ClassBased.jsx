f4473a02c1093f78367401cdaee1e22842791cee
Charlotte Lyons <charlottetlyons@gmail.com>
2025-07-16 15:00:00 -0500

    Class based setup state

--- JavaScript/React/react-playground/src/components/ClassBased.jsx
+++ JavaScript/React/react-playground/src/components/ClassBased.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { Component } from "react";
 
 class ClassBased extends Component {
+    constructor() {
+        super()
+        this.state = {}
+    }
+    
     render() {
         return <>Class Based</>
     }
