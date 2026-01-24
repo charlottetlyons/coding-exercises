67381bcf523386e0e4f4eb2abe6e35358557dbd9
Charlotte Lyons <charlottetlyons@gmail.com>
2026-01-24 11:39:17 -0600

    Generic Angular component

---
+++ JavaScript/Angular/angular-playground/src/app/generic-component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "generic-component",
+    standalone: true,
+    template: "some content"
+})
+export class GenericComponent {}
