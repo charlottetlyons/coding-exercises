0974c253fc95d74b4dc1f95ad76bc8d482695215
Charlotte Lyons <charlottetlyons@gmail.com>
2026-04-05 20:28:24 -0500

    Prototype pattern Java

---
+++ Java/java_playground/src/designpatterns/creational/prototype/Prototype.java
@@ -0,0 +1,18 @@
+package designpatterns.creational.prototype;
+
+public interface Prototype {
+    Prototype clone() ;
+}
+
+class ConcretePrototype implements Prototype {
+    private int value;
+
+    public ConcretePrototype(int v) {
+        value = v;
+    }
+
+    @Override
+    public Prototype clone() {
+        return new ConcretePrototype(value);
+    }
+}
